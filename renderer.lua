34dd0f6902b242a14fe9631d07e54fbf60cfbb0c
LAOnGithub <armiiigaming@gmail.com>
2020-12-29 14:33:44 +0100

    Update renderer.lua

--- renderer.lua
+++ renderer.lua
@@ -3,6 +3,7 @@
 --- A render loop can be created by doing renderer.add_callback(f: function)
 
 --- @field add_callback fun(f: function, identifier: string|nil)
+--- @field rectangle fun(x: number, y: number, w: number, h: number, r: number, g: number, b: number, a:number)
 renderer = {}
 
 local renderer_callbacks = {}
@@ -28,6 +29,10 @@ function renderer.add_callback(f, identifier)
         renderer_callbacks:insert(f)
     end
 end
+function renderer.remove_callback(identifier)
+    renderer_callbacks[identifier] = nil -- gay
+end
+
 function renderer.rectangle(x, y, w, h, r, g, b, a)
     if syn then
         table.insert(cached_drawings, create_drawing('Square', {
@@ -35,6 +40,8 @@ function renderer.rectangle(x, y, w, h, r, g, b, a)
             Position = Vector2.new(x, y),
             Color = Color3.fromRGB(r, g, b),
             Transparency = a / 255,
+            Filled = true,
+            Thickness = 1,
             Visible = true
         }))
     else
@@ -43,14 +50,52 @@ function renderer.rectangle(x, y, w, h, r, g, b, a)
             Position = Vector2.new(x, y),
             Color = Color3.fromRGB(r, g, b),
             Transparency = a / 255,
+            Filled = true,
+            Thickness = 1,
             Visible = true
         }))
     end
 end
+function renderer.outlined_circle(x, y, radius, sides, r, g, b, a)
+    table.insert(cached_drawings, create_drawing('Circle', {
+        Radius = radius,
+        NumSides = sides,
+        Position = Vector2.new(x, y),
+        Color = Color3.fromRGB(r, g, b),
+        Transparency = a / 255,
+        Thickness = 1,
+        Visible = true
+    }))
+end
+function renderer.circle(x, y, radius, sides, r, g, b, a)
+    table.insert(cached_drawings, create_drawing('Circle', {
+        Radius = radius,
+        NumSides = sides,
+        Position = Vector2.new(x, y),
+        Color = Color3.fromRGB(r, g, b),
+        Transparency = a / 255,
+        Filled = true,
+        Thickness = 1,
+        Visible = true
+    }))
+end
+function renderer.text(x, y, text, size, r, g, b, a)
+    table.insert(cached_drawings, create_drawing('Text', {
+        Text = text,
+        Position = Vector2.new(x, y),
+        Size = size,
+        Color = Color3.fromRGB(r, g, b),
+        Transparency = a / 255,
+        Visible = true
+    }))
+end
 
 game:GetService("RunService"):BindToRenderStep("renderer", 1, function()
     --// Put the last drawings into the correct table
-    last_frame_drawings = cached_drawings
+    last_frame_drawings = {table.unpack(cached_drawings)}
+
+    --// Clear the cached drawings
+    cached_drawings = {}
 
     --// Call the rendering callbacks
     for k, v in next, renderer_callbacks do
